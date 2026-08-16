165dbf7dca7bc353600613449dc1ce81c26b4018
SHWETA DEVRAM KOKATE <shwetakokate120@gmail.com>
2026-08-16 23:56:19 +0530

    Time: 53 ms (50.38%), Space: 30.8 MB (31.3%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        count = [0, 0, 0]
+
+        for stone in stones:
+            count[stone % 3] += 1
+
+        if count[0] % 2 == 0:
+            return min(count[1], count[2]) > 0
+
+        return abs(count[1] - count[2]) > 2
