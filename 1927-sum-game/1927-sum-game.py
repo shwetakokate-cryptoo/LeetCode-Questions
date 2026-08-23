1da0abbb22fae6faeb47267be511eec88f319bd2
SHWETA DEVRAM KOKATE <shwetakokate120@gmail.com>
2026-08-23 13:58:33 +0530

    Time: 53 ms (75.88%), Space: 19.9 MB (65.29%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num)
+        half = n // 2
+
+        left_sum = 0
+        right_sum = 0
+        left_q = 0
+        right_q = 0
+
+        for i in range(half):
+            if num[i] == '?':
+                left_q += 1
+            else:
+                left_sum += int(num[i])
+
+        for i in range(half, n):
+            if num[i] == '?':
+                right_q += 1
+            else:
+                right_sum += int(num[i])
+
+        if (left_q + right_q) % 2 == 1:
+            return True
+
+        return 2 * (left_sum - right_sum) != 9 * (right_q - left_q)
